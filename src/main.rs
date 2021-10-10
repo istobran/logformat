15f7165ec5b0b5536694cc96510341fecd929eea
BangZ <i@bangz.me>
2021-10-10 11:33:23 +0800

    added chrono dep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use chrono::prelude::*;
+
+fn format(format_str: &str) -> String {
+    let now: DateTime<Local> = Local::now();
+    return now.format(format_str).to_string();
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", format("%Y-%m-%d %H:%M:%S"));
 }
