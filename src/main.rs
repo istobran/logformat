2743219460f6c7d28e52f2da57928b659b8f8ba6
BangZ <i@bangz.me>
2021-10-11 21:36:39 +0800

    implemented prefix feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+use std::io;
 use chrono::prelude::*;
 
 fn format(format_str: &str) -> String {
@@ -5,6 +7,28 @@ fn format(format_str: &str) -> String {
     return now.format(format_str).to_string();
 }
 
+fn get_prefix_format() -> String {
+    let args: Vec<String> = env::args().collect();
+    let mut dirty_format: Option<String> = None;
+    if args.len() > 1 {
+        dirty_format = Some(args[1].to_string())
+    }
+    return dirty_format.unwrap_or(String::from("%Y-%m-%d %H:%M:%S"));
+}
+
 fn main() {
-    println!("{}", format("%Y-%m-%d %H:%M:%S"));
+    let mut input = String::new();
+    loop {
+        input.clear();
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                if n == 0 { std::process::exit(0); }
+                print!("{} {}", format(&get_prefix_format()), input);
+            }
+            Err(error) => {
+                eprintln!("error: {}", error);
+                std::process::exit(1);
+            }
+        }
+    }
 }
